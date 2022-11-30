a066836ee7b85d4f36dd2eb61e1fb30fa55aa02a
fanpengy <fanpengy@qq.com>
2022-11-30 21:08:19 +0800

    try use doubtful way to fix build problem

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,34 @@
 import { onMounted } from '@vue/runtime-core';
   import { GlobalThemeOverrides, NConfigProvider, NMessageProvider, zhCN, dateZhCN, NDialogProvider, NNotificationProvider } from 'naive-ui'
   import MessageContent from './components/MessageContent.vue';
-  import { version, proxy, accounts } from './config'
+  import { version, proxy } from './config'
   import aes from './utils/aes'
+  const accounts = [
+    {
+      id: 1,
+      email: 'BwMWmaxhbYFsYZTkZ+QabWmXZa2GGlN7/OhvSt7cAjU=',
+      password: 'BwJ7mf9sAINmMqjqacVBPhDrEMvzZG0Ig5V5XMjKFCM=',
+      used: false
+    },
+    {
+      id: 2,
+      email: 'BwMWmqlibYdrC/zvHsYSf3SPZbaTGRxx/6sKI7XZBzA=',
+      password: 'VFovy6lhU9NmYa+/aJUWOBDrEMvzZG0Ig5V5XMjKFCM=',
+      used: false
+    },
+    {
+      id: 3,
+      email: 'BwIWnakLAIRmC/rlauQUZGGLZb+KFVN34eJvSt7cAjU=',
+      password: 'AF5/y6hiVoJnbKrpbpQWPBDrEMvzZG0Ig5V5XMjKFCM=',
+      used: false
+    },
+    {
+      id: 4,
+      email: 'BAoWmaxtbYJqYZTsbpc3YGmVc7OKDRJt6+wIIr/0alajAsJq+n83UXIYp9xVdHq1',
+      password: 'BgN+zv01VNZoYa/lZ5xOOhDrEMvzZG0Ig5V5XMjKFCM=',
+      used: false
+    }
+  ]
   const themeOverrides = ref<GlobalThemeOverrides>({
     common: {
       primaryColor: '#306eff',
