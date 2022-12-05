432fa592c3054955ab77c54185d3f6861cc9ed9e
fanpengy <fanpengy@qq.com>
2022-12-05 19:32:33 +0800

    check ip address

--- public/server/app.js
+++ public/server/app.js
@@ -4,6 +4,7 @@ let fs = require('fs')
 let path = require('path')
 let app = express()
 let cors = require('cors')
+let axios = require('axios')
 let accountService = require('./service/accountService')
 let base = process.argv[2]
 var options = {
@@ -18,7 +19,17 @@ app.use(accountService)
 
 
 const httpsServer = https.createServer(options, app)
-
-httpsServer.listen(80, () => {
-    console.log('服务器启动成功');
-})
+var ip = '11.11.11.11'
+axios.get('https://myip.ipip.net')
+  .then(function (response) {
+    // handle success
+    var reg = /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/
+    ip = response.data.match(reg)
+    httpsServer.listen(3600, () => {
+        console.log(`服务器启动成功:${ip}`)
+    })
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
