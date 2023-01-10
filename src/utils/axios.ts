2296af36f765f48a6f0b33b842cb2ac0172369c9
fanpengy <fanpengy@qq.com>
2023-01-10 19:52:18 +0800

    change local database service port to 8081

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -173,7 +173,7 @@ const instance3 = axios.create({})
 instance3.interceptors.request.use(request => {
   var url = request.url
   console.log('url',url)
-  if(url && !url?.startsWith('https://localhost:3600')) {
+  if(url && !url?.startsWith('https://localhost:8081')) {
     const optimise = JSON.parse(localStorage.getItem('pikpakOptimize') || '{}')
     if(optimise.autoChangeAccount) {
       //https://encryptuyhasiuhsiusdecrypt/api/account_get
