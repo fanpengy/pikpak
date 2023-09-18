339452d9ef47e700801d42b31c5f718f2e222ce5
fanpengy <40634937+fanpengy@users.noreply.github.com>
2023-09-18 22:53:46 +0800

    Update proxy

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,12 +1,10 @@
 export const proxy = [
-  'https://diii.tk',
   'https://api-pikpak.tjsky.cf',
   'https://api-pikpak.tjsky-1.cf',
-  'https://cc123.cf',
-  'https://cf-api.xswd.cf',
   'https://wkr.btstream.net',
-  'https://pikpak.yang-s.online',
   'https://pikpak.520936.xyz',
+  'https://darazemon.xyz',
+  'https://pikpak.995579.xyz',
 ]
 
 export const version = '1.25.2'
