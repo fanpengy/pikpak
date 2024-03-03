22be30824b258a5020175f6bd8514bc2e4ef48d9
fanpengy <fanpengy@qq.com>
2024-03-03 22:49:58 +0800

    change proxy

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,12 @@
 export const proxy = [
-  'https://api-pikpak.tjsky.cf',
-  'https://api-pikpak.tjsky-1.cf',
+  // 'https://api-pikpak.tjsky.cf',
+  // 'https://api-pikpak.tjsky-1.cf',
+  // 'https://wkr.btstream.net',
+  // 'https://pikpak.520936.xyz',
+  // 'https://darazemon.xyz',
+  // 'https://pikpak.995579.xyz',
   'https://wkr.btstream.net',
-  'https://pikpak.520936.xyz',
-  'https://darazemon.xyz',
-  'https://pikpak.995579.xyz',
+  'https://pikpak.995579.xyz'
 ]
 
 export const version = '1.25.2'
